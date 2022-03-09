4e29d62b3185f1bb28c12e8e601b2f88ef0d8a39
Joseph Nartey <jnartey2022@columbiasecondary.org>
2022-03-08 22:07:34 -0500

    Update app.py

--- assets/app.py
+++ assets/app.py
@@ -1,1 +1,10 @@
 #test to make sure Joseph N has connected to repository
+
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
+
+#still troubleshooting, app wont launch on my end
