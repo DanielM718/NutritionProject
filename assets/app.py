6654fd63ff3994c3f036f723b87b6295a8931462
Joseph Nartey <jnartey2022@columbiasecondary.org>
2022-03-29 16:23:02 -0400

    Adding code and what not

--- assets/app.py
+++ assets/app.py
@@ -1,10 +1,20 @@
 #test to make sure Joseph N has connected to repository
-import React from 'react'
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 #still troubleshooting, app wont launch on my end
