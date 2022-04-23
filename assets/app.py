4c93b1c69cffbc006f9e4b998af70c8e3705b0ce
Joseph Nartey <jnartey2022@columbiasecondary.org>
2022-04-23 13:31:15 -0400

    Bootstrap stuff now works, we have a working navbar

--- assets/app.py
+++ assets/app.py
@@ -1,4 +1,4 @@
-#test to make sure Joseph N has connected to repository
+
 from flask import Flask, render_template, app
 
 
@@ -10,17 +10,15 @@ def start():
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About - Nutrition Project')
+    return render_template('about.html')
 
 @app.route('/login')
 def login():
-   return render_template('login.html', title='Login - Nutrition Project')
+   return render_template('login.html')
 
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home - Nutrition Project')
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-#still troubleshooting, app wont launch on my end
