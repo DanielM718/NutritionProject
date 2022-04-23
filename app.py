c9c8b4dcf0938efe9c789e569717e98eb2e1e095
Nana-Okrah <nanaokrah5@gmail.com>
2022-04-23 17:06:20 +0100

    signup page is functioning with firebase

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email.message import EmailMessage
 from re import template
 from flask import Flask, render_template, request, current_app as app 
 from flask_cors import CORS
@@ -10,7 +11,7 @@ from firebase_admin import credentials, firestore
 
 
 
-cred = credentials.Certificate('nutrition-app-ba2a6-firebase-adminsdk-419j0-8a54f8eead.json')
+cred = credentials.Certificate('nutrition-app-ba2a6-firebase-adminsdk-419j0-3336f9c6a5.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -50,7 +51,7 @@ def sign_up_page():
 
     }
     username = db.collection('user_info')#access user_info collection
-    user = username.document(user_n).set(user_dict())#gets username
+    user = username.document(user_n).set(user_dict())#adds data passed to firebase DB
     
     #login_username= request.form['']
     #login_password= request.form['']
@@ -64,9 +65,14 @@ def sign_up_page():
     #return render_template('index.html', username=username, user=user)
 
 
-@app.route('/api/login', methods=['GET','POST'])
+@app.route('/login',methods=['GET','POST'])
 def login():
-    if Sign_up.username == request.form['username'] and Sign_up.password == request.form['password']:
+    #  login_username= request.form['login_username']
+    #  login_password= request.form['login_password']
+
+    #  if db.username == login_username and db.password== login_password:
+    #     return render_template('test.html',login_username = login_username, login_password=login_password)
+    #  else:
         return render_template('login.html')
     
 
