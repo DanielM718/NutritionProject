5efd74c596c6e11591a3cbacfc0fe0d459d8a6a7
Joseph Nartey <jnartey2022@columbiasecondary.org>
2022-06-04 13:59:20 -0400

    Using Bryan's way to implement the camera
    Using cv2

--- assets/app.py
+++ assets/app.py
@@ -1,7 +1,19 @@
 
 from flask import Flask, render_template, app, cv2, jsonify, request, make_response, abort 
+camera = cv2.VideoCapture(0)
 
-
+def gen_frames():
+    # generate frame by frame from camera
+    while True:
+        # Capture frame-by-frame
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
 app = Flask(__name__)
@@ -21,7 +33,10 @@ def login():
 def home():
     return render_template('home.html')
 
-@app.route('')
+@app.route('/video_feed')
+def video_feed():
+        #Video streaming route. Put this in the src attribute of an img tag
+    return Response (gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug=True)
